c941c793c7ab667ccecd1610eb32c6f28aef47fe
Holder-hash <X-Lamamen-X@yandex.ru>
2023-09-09 19:44:11 +0300

    path

--- js/script.js
+++ js/script.js
@@ -98,7 +98,7 @@ function game() {
         ctx.fillStyle = "#000";
         ctx.font = "10px Verdana";
         ctx.fillText("Счет: " + score, 10, 15);
-        ctx.fillText("Лучший счет: " + localStorage.getItem('bestScore'), canvas.width - 90, 15);
+        ctx.fillText("Лучший счет: " + localStorage.getItem('bestScore'), canvas.width - 89, 15);
     
     requestAnimationFrame(game)
 }
